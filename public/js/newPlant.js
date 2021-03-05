ca42b2d0584cacbc3cbe912004009bec0e7cee90
Skylar Yi Li <yil067@ucsd.edu>
2021-03-05 11:15:33 +0800

    part

--- public/js/newPlant.js
+++ public/js/newPlant.js
@@ -6,3 +6,15 @@ document.getElementsByName("date")[0].setAttribute('max', today);
 
 
 
+function imagefun() {
+            var Image_Id = document.getElementById('flower');
+            if (Image_Id.src.match("/images/add-photo.svg")) {
+                Image_Id.src = 'https://loremflickr.com/320/240/plant';
+            }
+            if (Image_Id.src.match('https://loremflickr.com/320/240/plant')) {
+                Image_Id.src = 'https://loremflickr.com/320/240/flower';
+            }
+      
+            else {
+                Image_Id.src = "https://loremflickr.com/320/240/plant";}}
+
