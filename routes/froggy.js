c8b458d510e60766eb5fea67cea5dbd36eb7e707
Jennifer Qi <jeqi@ucsd.edu>
2021-03-04 22:23:21 -0800

    test

--- routes/froggy.js
+++ routes/froggy.js
@@ -156,3 +156,67 @@ exports.addEntry = function(req, res) { 
   		"age": ageDays
 	});
  }
+
+//SECOND FORM
+exports.update = function(request, response) {    
+	// Your code goes here
+	var inputDate = request.query.sdate;
+	var month = inputDate.substring(5, 7);
+	var day = inputDate.substring(8);
+	var year = inputDate.substring(0, 4);
+	var outputDate = month + "/" + day + "/" + year;
+
+	var howOften = request.query.remind;
+	var timeDay = request.query.time;
+	var timeHour = timeDay.substring(0,2);
+	var timeRest = timeDay.substring(2);
+	if (timeHour > 12) {
+		timeHour = timeHour - 12 + timeRest + " PM";
+	}
+	else if (timeHour == 12) {
+		timeHour = timeHour + timeRest + " PM";
+	}
+	else if (timeHour == 0) {
+		timeHour = 12 + timeRest + " AM";
+	}
+	else {
+		timeHour = timeHour + timeRest + " AM";
+	}
+
+	if (timeHour.length < 8) {
+		timeHour = "0" + timeHour;
+	}
+
+	var watering = timeHour + " " + howOften;
+
+	//done calculating variables
+	var viewPlant = req.params.plantName;
+	var accessPic = "edit-photo-button.svg"; 
+	var accessSpecies = "placeholder species";
+
+	for (var i = 0; i < myPlantsData["Plants"].length; i++) {
+		if (myPlantsData["Plants"][i]["nickname"] == viewPlant) {
+			myPlantsData["Plants"][i].watering = watering;
+			myPlantsData["Plants"][i].start = start;
+
+			accessPic = myPlantsData["Plants"][i]["pic"];
+			accessSpecies = myPlantsData["Plants"][i]["species"];
+			break;
+		}
+	}
+	//get vars for rendering
+	var startDate = new Date(start);
+	var dateCurr = new Date(); //UTC timezone
+
+	var plantAge = dateCurr.getTime() - startDate.getTime();
+	var ageDays = plantAge/(1000 * 3600 * 24);
+	ageDays = Math.floor(ageDays);
+
+	response.render('froggy', {
+  		"nickname": viewPlant,
+	  	"pic": accessPic,
+	  	"species": accessSpecies,
+	  	"age": ageDays,
+	  	"start": start,
+  	});
+ }
